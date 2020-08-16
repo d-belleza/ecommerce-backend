b21c195f4c48f8ec99eb6a1f4b84291745cedbaf
davidbelleza <davidbelleza@gmail.com>
2020-08-16 16:48:07 -0500

    create API routes

--- models/Product.js
+++ models/Product.js
@@ -21,7 +21,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2), // decimal to 2 places
       allowNull: false,
       validate: {
         isDecimal: true
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,63 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(results => res.json(results))
+  .catch(err => res.status(500).json(err))
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(results => res.json(results))
+  .catch(err => res.status(404).json(err))
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then(results => res.json(results))
+  .catch(err => res.status(404).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(results => res.json(results))
+  .catch(err => res.status(404).json(err))
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(results => res.json(results))
+  .catch(err => res.status(404).json(err))
 });
 
 module.exports = router;
